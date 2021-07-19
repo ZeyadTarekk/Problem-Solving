1cae62dcd7d796619ec177a5a3d5c3f805845bb4
ZeyadTa <Zeyad.ta01@gmail.com>
2021-07-19 02:54:39 +0200

    Create 5-Beautiful-Matrix.cpp

---
+++ MostafaSaad Sheet/5-Beautiful-Matrix.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,i,j;
+  for(i=1;i<=5;i++)
+    for(j=1;j<=5;j++)
+      {
+        cin>>x;
+        if(x==1)
+          break;
+      }
+  cout<<abs(i-3)+abs(j-3);
+}
