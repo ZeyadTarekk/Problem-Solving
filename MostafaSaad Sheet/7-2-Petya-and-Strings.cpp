1382c0d3ec16754c1b67ce95bcf2fd5d22b579de
ZeyadTa <Zeyad.ta01@gmail.com>
2021-07-19 03:47:08 +0200

    Create 7-2-Petya-and-Strings.cpp
    This Solution is worse the the first in Time complexity but without using transform

---
+++ MostafaSaad Sheet/7-2-Petya-and-Strings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string s1,s2;
+  cin>>s1>>s2;
+  for(int i=0;i<s1.size();i++)
+    s1[i]=tolower(s1[i]);
+  for(int i=0;i<s2.size();i++)
+    s2[i]=tolower(s2[i]);
+  if(s1<s2)
+    cout<<-1;
+  else if(s1>s2)
+    cout<<1;
+  else
+    cout<<0;
+}
