20d570ac357356f9579064005e7377b1309144ec
ZeyadTa <Zeyad.ta01@gmail.com>
2021-07-19 03:23:28 +0200

    Update 5-Beautiful-Matrix.cpp

--- MostafaSaad Sheet/5-Beautiful-Matrix.cpp
+++ MostafaSaad Sheet/5-Beautiful-Matrix.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 int main()
 {
-  int x,i,j;
-  for(i=1;i<=5;i++)
-    for(j=1;j<=5;j++)
-      {
-        cin>>x;
-        if(x==1)
-          break;
-      }
-  cout<<abs(i-3)+abs(j-3);
+	int x, i, j;
+	for (i = 1; i <= 5; i++)
+	{
+		for (j = 1; j <= 5; j++)
+		{
+			cin >> x;
+			if (x == 1)
+				break;
+		}
+		if (x == 1)
+			break;
+	}
+	cout << abs(i - 3) + abs(j - 3);
 }
