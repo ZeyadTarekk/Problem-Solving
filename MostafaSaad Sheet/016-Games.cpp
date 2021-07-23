04224063a222d037951f50cb7c274242d56961c1
ZeyadTa <Zeyad.ta01@gmail.com>
2021-07-23 03:30:09 +0200

    Create 016-Games.cpp

---
+++ MostafaSaad Sheet/016-Games.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+int main()
+{
+  int n;
+  int no=0;
+  cin>>n;
+  int **arr = new int*[n];
+  for(int i=0;i<n;i++)
+    {
+      arr[i]=new int[2];
+    }
+  for(int i=0;i<n;i++)
+    for(int j=0;j<2;j++)
+      cin>>arr[i][j];
+  for(int i=0;i<n;i++) 
+    for(int j=0;j<n;j++)
+      if(j!=i)
+        if(arr[i][1]==arr[j][2])
+          no++;
+  cout<<no;
+}
