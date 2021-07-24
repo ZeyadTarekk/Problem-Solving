04ba7cecf1b86936f92d84827689e1444812ea4d
ZeyadTa <Zeyad.ta01@gmail.com>
2021-07-24 21:19:24 +0200

    Create 020-Die-Roll.cpp

---
+++ MostafaSaad Sheet/020-Die-Roll.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  int y,w;
+  cin>>y>>w;
+  int x=6-max(y,w)+1;
+  if(x==2)
+    cout<<1<<'/'<<3;
+  else if(x==3)
+    cout<<1<<'/'<<2;
+  else if(x==4)
+    cout<<2<<'/'<<3;
+  else if(x==6)
+    cout<<1<<'/'<<1;
+  else
+    cout<<x<<'/'<<6;
+    return 0;
+}
