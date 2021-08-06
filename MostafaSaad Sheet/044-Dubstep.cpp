208ea1524bb699adae13cac5f4d7a9a892c45530
ZeyadTa <Zeyad.ta01@gmail.com>
2021-08-07 01:46:11 +0200

    Create 044-Dubstep.cpp

---
+++ MostafaSaad Sheet/044-Dubstep.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+		{
+			i += 2;
+			cout << " ";
+		}
+		else
+			cout << s[i] ;
+	}
+}
