66738b7df2420663c88e3b658c87787ffd3d875e
Zeyad Tarek <Zeyad.ta01@gmail.com>
2023-04-23 18:27:00 +0200

    Time: 125 ms (36.82%), Space: 68.5 MB (80.79%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    int findMax(vector<int>&arr,int begin,int end){
-        int maxNo = arr[begin];
-        for(int i=begin+1;i<end;i++){
-            if(arr[i]>maxNo)
-                maxNo = arr[i];
-        }
-        return maxNo;
-    }
     vector<int> replaceElements(vector<int>& arr) {
         int len = arr.size();
-        vector<int> maxArr(len);
-        maxArr[len - 1] = arr[len - 1];
+        int maxNo = INT_MIN;
+        int temp = arr[len-1];
         for(int i=len - 2;i>=0;i--){
-            maxArr[i] = max(maxArr[i+1],arr[i]);
-        }
-
-        for(int i=0;i<len -1;i++){
-            arr[i] = maxArr[i+1];
+            maxNo = max(maxNo,temp);
+            // cout<<maxNo<<" "<<arr[i+1]<<endl;
+            temp=arr[i];
+            arr[i] = maxNo;
         }
         arr[len - 1] = -1;
         return arr;
