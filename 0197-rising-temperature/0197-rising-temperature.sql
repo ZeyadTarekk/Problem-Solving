3cda127af425b1bdff0e772381df4a23495f578d
Zeyad Tarek <Zeyad.ta01@gmail.com>
2023-05-02 09:03:41 +0300

    Time: 1300 ms (6.75%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-SELECT DISTINCT fir.id
-FROM weather as fir, weather AS sec
-WHERE fir.temperature > sec.temperature 
-AND DATEDIFF(fir.recordDate,sec.recordDate) = 1
+# SELECT DISTINCT fir.id
+# FROM weather AS fir, weather AS sec
+# WHERE fir.temperature > sec.temperature 
+# AND DATEDIFF(fir.recordDate,sec.recordDate) = 1
+
+SELECT fir.id
+FROM weather AS fir
+WHERE fir.temperature > (
+    SELECT sec.temperature 
+    FROM weather AS sec 
+    WHERE DATEDIFF(fir.recordDate,sec.recordDate)=1 
+    )
