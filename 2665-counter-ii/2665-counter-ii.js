ac2ca39cf552e8c7785c54ebdfa445ec8a30c0d6
Zeyad Tarek <Zeyad.ta01@gmail.com>
2023-05-07 03:31:53 +0300

    Time: 71 ms (38.48%), Space: 44.6 MB (74.53%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initial = init;
+    let cur=init;
+    return {
+     increment: function increment(){
+        return ++cur;
+    },
+    decrement: function decrement(){
+        return --cur;
+    },
+     reset: function reset(){
+         cur = init;
+        return cur;
+    }
+}
+    
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
