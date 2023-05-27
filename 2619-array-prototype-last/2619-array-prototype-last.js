55d7c14bb235620d2bc76e120064184d28a97d72
Zeyad Tarek <Zeyad.ta01@gmail.com>
2023-05-27 19:02:32 +0300

    Time: 58 ms (49.26%), Space: 41.5 MB (91.50%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,11 @@
+Array.prototype.last = function() {
+    const len = this.length;
+    if(len ===0)
+        return -1;
+    return this[len-1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
